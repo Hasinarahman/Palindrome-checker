c5f89056fb9e8d05a8e686bdb83e4461c41e7c2c
Hasinarahmani <107977511+Hasinarahmani@users.noreply.github.com>
2024-06-27 20:51:55 +0430

    another solve the linter problem

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ checkButton.addEventListener('click', () => {
   const lowerReplaced = textInput.value.toLowerCase().replace(/[^a-z0-9]/g, '');
 
   if (textInput.value === '') {
-    return window.alert('Please input a value');
+    alert('Please input a value');
   } else if (textInput.value.length === 1) {
     result.innerText = `${textInput.value} is a palindrome`;
-  }
-  else if (lowerReplaced === [...lowerReplaced].reverse().join('')) {
+  } else if (lowerReplaced === [...lowerReplaced].reverse().join('')) {
     result.innerText = `${textInput.value} is a palindrome`;
   } else {
     result.innerText = `${textInput.value} is not a palindrome`;
