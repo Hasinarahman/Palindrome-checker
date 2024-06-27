6d763726e7145c06b0503cda0d5b5fbb9f52c6c3
Hasinarahmani <107977511+Hasinarahmani@users.noreply.github.com>
2024-06-27 20:42:09 +0430

    solving the js linter error

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const textInput = document.getElementById("text-input");
-const checkButton = document.getElementById("check-btn");
-const result = document.getElementById("result");
+const textInput = document.getElementById('text-input');
+const checkButton = document.getElementById('check-btn');
+const result = document.getElementById('result');
 
 checkButton.addEventListener('click', () => {
-  const lowerReplaced = textInput.value.toLowerCase().replace(/[^a-z0-9]/g,"")
+  const lowerReplaced = textInput.value.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-   if (textInput.value === '') {
-    return window.alert("Please input a value");
+  if (textInput.value === '') {
+    return window.alert('Please input a value');
   } else if (textInput.value.length === 1) {
-result.innerText = `${textInput.value} is a palindrome`
-  } 
-  else if (lowerReplaced === [...lowerReplaced].reverse().join("")){
-    result.innerText = `${textInput.value} is a palindrome`
+    result.innerText = `${textInput.value} is a palindrome`;
+  }
+  else if (lowerReplaced === [...lowerReplaced].reverse().join('')) {
+    result.innerText = `${textInput.value} is a palindrome`;
   } else {
-    result.innerText = `${textInput.value} is not a palindrome`
+    result.innerText = `${textInput.value} is not a palindrome`;
   }
 });
